8c3be12fd4147b708876ea6ee1e5077e7daaf3fd
root <root@fullstack-vm.v3yxpm04w2yezfyemnuiopn4xa.bx.internal.cloudapp.net>
2024-02-08 11:40:31 +0000

    intial

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    userBaseUrl: 'http://localhost:5004',
+    blogBaseUrl: 'http://localhost:5002',
+    commentBaseUrl: 'http://localhost:5003',
+    imageBaseUrl: 'http://localhost:5005'
+  };
